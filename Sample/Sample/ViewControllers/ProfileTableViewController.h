5a2900c4eaaedf920799829bf6aae2c17acdd2d4
Krzysztof Kurzawa <krzysztof.kurzawa@toreforge.com>
2018-04-10 05:28:30 +0100

    Sample app added

---
+++ Sample/Sample/ViewControllers/ProfileTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileTableViewController.h
+//  Sample
+//
+//  Created on 30/11/2017.
+//  Copyright © 2017 Synerise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
