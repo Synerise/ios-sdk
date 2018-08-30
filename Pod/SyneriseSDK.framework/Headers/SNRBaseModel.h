e89c8e8435f86a2003924cc21e2973f973993c07
Krzysztof <napisz@krzysztofkurzawa.com>
2018-08-30 14:08:59 +0200

    Update framework version

---
+++ Pod/SyneriseSDK.framework/Headers/SNRBaseModel.h
@@ -0,0 +1,15 @@
+// 
+//  SNRBaseModel.h
+//  SyneriseSDK
+//
+//  Created by Synerise
+//  Copyright (c) 2018 Synerise. All rights reserved.
+//
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SNRBaseModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
