d5f665f55469be37167151e2350d862d8260d850
Krzysztof Kurzawa <napisz@krzysztofkurzawa.com>
2021-12-14 12:44:28 +0100

    Update Sample App Swift

--- SampleAppSwift/SampleAppSwift/Main/Developer Tools Flow/ViewControllers/ClientAPI/Authorization/ClientAuthorizationViewController.swift
+++ SampleAppSwift/SampleAppSwift/Main/Developer Tools Flow/ViewControllers/ClientAPI/Authorization/ClientAuthorizationViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 import SyneriseSDK
 
 class ClientAuthorizationViewController: DefaultViewController {
     
     // MARK: - IBAction
     
-    @IBAction func signOutButtonWasPressed(_ sender: DefaultButton) {
-        Client.signOut()
+    @IBAction func authenticateByFacebook() {
+        guard let facebookToken = AccessToken.current?.tokenString else {
+            return
+        }
+        
+        Client.authenticateByFacebookIfRegistered(facebookToken: facebookToken, authID: "", success: { isSuccess in
+            if isSuccess {
+                self.hideLoading()
+                self.showSuccessInfo()
+            }
+        }) { (error) in
+            let agreements: ClientAgreements = ClientAgreements()
+            
+            let context: ClientFacebookAuthenticationContext = ClientFacebookAuthenticationContext()
+            context.agreements = agreements
+            context.attributes = ["param": "value"]
+            
+            Client.authenticateByFacebook(facebookToken: facebookToken, authID: "", context: context, success: { isSuccess in
+                if isSuccess {
+                    self.hideLoading()
+                    self.showSuccessInfo()
+                }
+            }, failure: { error in
+                self.hideLoading()
+                self.showErrorInfo(error as NSError)
+            })
+        }
     }
     
     // MARK: - Lifecycle
