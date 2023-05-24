2989b9eb6fdc9797b77116b4d0d84f898bc5dd93
Krzysztof Kurzawa <krzysztof.kurzawa@synerise.com>
2023-05-24 23:34:17 +0200

    Release of version 4.13.0

---
+++ SDK/XCFramework/SyneriseSDK.xcframework/ios-arm64/SyneriseSDK.framework/Headers/SNRScreenViewAudienceInfo.h
@@ -0,0 +1,26 @@
+//
+//  SNRScreenViewAudienceInfo.h
+//  SyneriseSDK
+//
+//  Created by Synerise
+//  Copyright (c) 2023 Synerise. All rights reserved.
+//
+
+#import <SyneriseSDK/SNRBaseModel.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * @class SNRScreenViewAudienceInfo
+ */
+
+NS_SWIFT_NAME(ScreenViewAudienceInfo)
+@interface SNRScreenViewAudienceInfo : SNRBaseModel
+
+@property (copy, nonatomic, nullable, readonly) NSArray<NSString *> *segments;
+@property (copy, nonatomic, nullable, readonly) NSString *query;
+@property (copy, nonatomic, nullable, readonly) NSString *targetType;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- SDK/XCFramework/SyneriseSDK.xcframework/ios-arm64_x86_64-simulator/SyneriseSDK.framework/Headers/SNRContent.h
+++ SDK/XCFramework/SyneriseSDK.xcframework/ios-arm64_x86_64-simulator/SyneriseSDK.framework/Headers/SNRContent.h
@@ -6,10 +6,12 @@
 //  Copyright (c) 2023 Synerise. All rights reserved.
 //
 
+@class SNRDocument;
 @class SNRDocumentsApiQuery;
 @class SNRRecommendationOptions;
 @class SNRRecommendationResponse;
 @class SNRScreenViewResponse;
+@class SNRScreenView;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -29,10 +31,24 @@ NS_SWIFT_NAME(Content)
  * @param slug Identifies a specific document.
  * @param success A block object to be executed when the operation finishes successfully.
  * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ * 
+ * @deprecated Deprecated in version 4.13.0
  */
 + (void)getDocument:(NSString *)slug
             success:(void (^)(NSDictionary *document))success
-            failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getDocument(slug:success:failure:));
+            failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getDocument(slug:success:failure:)) DEPRECATED_MSG_ATTRIBUTE("Use `Content.generateDocument(slug:success:failure:)` instead.");
+
+/**
+ * Generates the document that is defined for the provided slug.
+ *
+ * @param slug Identifies a specific document.
+ * @param success A block object to be executed when the operation finishes successfully.
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ */
++ (void)generateDocument:(NSString *)slug
+                 success:(void (^)(SNRDocument *document))success
+                 failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(generateDocument(slug:success:failure:));
+
 
 /**
  * Gets documents that are defined for parameters provided in the query object.
@@ -40,10 +56,25 @@ NS_SWIFT_NAME(Content)
  * @param apiQuery `SNRDocumentsApiQuery` object responsible for storing all query parameters.
  * @param success A block object to be executed when the operation finishes successfully.
  * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ *
+ * @deprecated Deprecated in version 4.13.0
  */
 + (void)getDocumentsWithApiQuery:(SNRDocumentsApiQuery *)apiQuery
-                          success:(void (^)(NSArray *documents))success
-                          failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getDocuments(apiQuery:success:failure:));
+                         success:(void (^)(NSArray *documents))success
+                         failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getDocuments(apiQuery:success:failure:)) DEPRECATED_MSG_ATTRIBUTE("This method is deprecated.");
+
+/**
+ * Gets recommendations that are defined for the options provided.
+ *
+ * @param options `SNRRecommendationOptions` object providing parameters for recommendations.
+ * @param success A block object to be executed when the operation finishes successfully.
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ *
+ * @deprecated Deprecated in version 4.13.0
+ */
++ (void)getRecommendations:(SNRRecommendationOptions *)options
+                   success:(void (^)(SNRRecommendationResponse *recommendationResponse))success
+                   failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getRecommendations(options:success:failure:)) DEPRECATED_MSG_ATTRIBUTE("Use `Content.getRecommendationsV2(options:success:failure:)` instead.");
 
 /**
  * Gets recommendations that are defined for the options provided.
@@ -52,18 +83,31 @@ NS_SWIFT_NAME(Content)
  * @param success A block object to be executed when the operation finishes successfully.
  * @param failure A block object to be executed when the operation finishes unsuccessfully.
  */
-+ (void)getRecommendations:(SNRRecommendationOptions *)options
-            success:(void (^)(SNRRecommendationResponse *recommendationResponse))success
-            failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getRecommendations(options:success:failure:));
++ (void)getRecommendationsV2:(SNRRecommendationOptions *)options
+                     success:(void (^)(SNRRecommendationResponse *recommendationResponse))success
+                     failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getRecommendationsV2(options:success:failure:));
 
 /**
  * Gets customer's highest-priority screen view campaign.
  *
  * @param success A block object to be executed when the operation finishes successfully.
  * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ *
+ * @deprecated Deprecated in version 4.13.0
  */
 + (void)getScreenViewWithSuccess:(void (^)(SNRScreenViewResponse *screenViewResponse))success
-                         failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getScreenView(success:failure:));
+                         failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(getScreenView(success:failure:)) DEPRECATED_MSG_ATTRIBUTE("Use `Content.generateScreenView(slug:success:failure:)` instead.");
+
+/**
+ * Generates customer's highest-priority screen view campaign that is defined for the provided slug.
+ *
+ * @param feedSlug Identifies a specific screen view.
+ * @param success A block object to be executed when the operation finishes successfully.
+ * @param failure A block object to be executed when the operation finishes unsuccessfully.
+ */
++ (void)generateScreenView:(NSString *)feedSlug
+                   success:(void (^)(SNRScreenView *screenView))success
+                   failure:(void (^)(SNRApiError *error))failure NS_SWIFT_NAME(generateScreenView(feedSlug:success:failure:));
 
 @end
 
